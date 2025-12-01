f5ceb96322533ed4d6c3e5e09dbdb81874015c89
Shaun Kiszonas <shadoewraith99@gmail.com>
2025-11-30 18:30:20 -0800

    updated file creation

--- create_files.sh
+++ create_files.sh
@@ -5,9 +5,11 @@ for y in `seq -w 2025 2025`; do
 
   for i in `seq -w 1 12`; do
     mkdir $y/day-$i
+    mkdir $y/day-$i/part-1
+    mkdir $y/day-$i/part-2
     touch $y/day-$i/README.md
-    touch $y/day-$i/day-$i-part-1.go
-    touch $y/day-$i/day-$i-part-2.go
-    touch $y/day-$i/day-$i-input.txt
+    touch $y/day-$i/part-1/main.go
+    touch $y/day-$i/part-2/main.go
+    touch $y/day-$i/input.txt
   done
 done
