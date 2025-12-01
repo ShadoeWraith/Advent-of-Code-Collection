d8e6678ec4f8c5b151578ab78b39c5cff5daf19d
Shaun Kiszonas <shadoewraith99@gmail.com>
2025-12-01 00:16:31 -0800

    once again changed file structure

--- 2025/day-01/main.go
+++ 2025/day-01/main.go
@@ -26,12 +26,47 @@ func main() {
 		records = append(records, line)
 	}
 
-	p, _ := findPassword(records)
+	p1, _ := partOne(records)
 
-	fmt.Printf("Password: %d\n", p)
+	p2, _ := partTwo(records)
+
+	fmt.Printf("Password for Part 1: %d\n", p1)
+	fmt.Printf("Password for Part 2: %d\n", p2)
 }
 
-func findPassword(records []string) (int, error) {
+// Part One =====================================================
+
+func partOne(records []string) (int, error) {
+	res := 0
+	cur := 50
+
+	for i := range records {
+		line := records[i]
+		val, _ := strconv.Atoi(line[1:])
+
+		val %= 100
+		if line[0] == 'L' {
+			val = -val
+		}
+
+		cur += val
+
+		if cur > 99 {
+			cur -= 100
+		} else if cur < 0 {
+			cur += 100
+		}
+		if cur == 0 {
+			res++
+		}
+	}
+
+	return res, nil
+}
+
+// Part Two =====================================================
+
+func partTwo(records []string) (int, error) {
 	res := 0
 	cur := 50
 	prev := 0
--- create_files.sh
+++ create_files.sh
@@ -5,11 +5,8 @@ for y in `seq -w 2025 2025`; do
 
   for i in `seq -w 1 12`; do
     mkdir $y/day-$i
-    mkdir $y/day-$i/part-1
-    mkdir $y/day-$i/part-2
     touch $y/day-$i/README.md
-    touch $y/day-$i/part-1/main.go
-    touch $y/day-$i/part-2/main.go
+    touch $y/day-$i/main.go
     touch $y/day-$i/input.txt
   done
 done
