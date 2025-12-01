fa8c5e47c3ed2ea567a2798ec35ead73cd532405
Shaun Kiszonas <shadoewraith99@gmail.com>
2025-11-30 23:44:29 -0800

    Day 1 complete

--- 2025/day-01/part-1/main.go
+++ 2025/day-01/part-1/main.go
@@ -1,5 +1,70 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+func main() {
+	filePath := "input.txt"
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("Error reading from file, %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var rotations []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		rotations = append(rotations, line)
+	}
+
+	p, err := findPassword(rotations)
+	if err != nil {
+		fmt.Printf("Error acquiring password, %v\n", err)
+		return
+	}
+
+	fmt.Printf("Password: %d\n", p)
+}
+
+func findPassword(rotations []string) (int, error) {
+	if len(rotations) == 0 {
+		return 0, fmt.Errorf("no rotations")
+	}
+
+	password := 0
+	currentPos := 50
+
+	for i := range rotations {
+		line := rotations[i]
+		dir := line[0]
+		numStr := line[1:]
+		num, _ := strconv.Atoi(numStr)
+
+		num %= 100
+		if dir == 'L' {
+			num = -num
+		}
+
+		currentPos += num
+
+		if currentPos > 99 {
+			currentPos -= 100
+		} else if currentPos < 0 {
+			currentPos += 100
+		}
+		if currentPos == 0 {
+			password++
+		}
+	}
+
+	return password, nil
 }
--- 2025/day-01/part-2/main.go
+++ 2025/day-01/part-2/main.go
@@ -1,5 +1,70 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+func main() {
+	filePath := "input.txt"
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("Error reading from file, %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var rotations []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		rotations = append(rotations, line)
+	}
+
+	p, _ := findPassword(rotations)
+
+	fmt.Printf("Password: %d\n", p)
+}
+
+func findPassword(rotations []string) (int, error) {
+	res := 0
+	cur := 50
+	prev := 1
+
+	for i := range rotations {
+		line := rotations[i]
+		val, _ := strconv.Atoi(line[1:])
+
+		res += val / 100
+
+		val %= 100
+		if line[0] == 'L' {
+			val = -val
+		}
+
+		cur += val
+
+		if cur > 99 {
+			cur -= 100
+		} else if cur < 0 {
+			cur += 100
+		}
+
+		switch {
+		case cur == 0:
+			res++
+		case cur >= prev && line[0] == 'L' && prev != 0:
+			res++
+		case cur <= prev && line[0] == 'R' && prev != 0:
+			res++
+		}
+		prev = cur
+	}
+
+	return res, nil
 }
