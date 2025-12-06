95e94bd5a71b1aa7a9b515a80c43fb4097369c1a
Shaun Kiszonas <shadoewraith99@gmail.com>
2025-12-06 10:13:36 -0800

    cleaned up code a bit

--- 2025/day-06/main.go
+++ 2025/day-06/main.go
@@ -8,57 +8,31 @@ import (
 	"strings"
 )
 
-func isNumericLine(record string) bool {
-	fields := strings.Fields(record)
-	if len(fields) == 0 {
-		return false
-	}
-
-	for _, field := range fields {
-		if _, err := strconv.Atoi(field); err != nil {
-			return false
-		}
-	}
-	return true
-}
-
 func main() {
 	records := utils.ReadFromFile()
 	var numericData solutions.Calculation
 
 	for _, record := range records {
+		if strings.ContainsAny(record, "+*") {
+			operators := strings.SplitSeq(record, " ")
 
-		if isNumericLine(record) {
-
-			numberStrings := strings.Split(record, " ")
-			var currentLineNumbers []int
-
-			for _, s := range numberStrings {
-				s = strings.TrimSpace(s)
-				if s == "" {
-					continue
-				}
-
-				num, _ := strconv.Atoi(s)
-				currentLineNumbers = append(currentLineNumbers, num)
+			for op := range operators {
+				numericData.Operations = append(numericData.Operations, op)
 			}
-
-			if len(currentLineNumbers) > 0 {
-				numericData.Numbers = append(numericData.Numbers, currentLineNumbers)
-			}
-
-		} else if strings.ContainsAny(record, "+*") {
-			operators := strings.Split(record, " ")
-
-			for _, op := range operators {
-				op = strings.TrimSpace(op)
-
-				if op == "+" || op == "*" {
-					numericData.Operations = append(numericData.Operations, op)
-				}
-			}
+			continue
 		}
+
+		numberStrings := strings.Split(record, " ")
+		var currentLineNumbers []int
+
+		for _, s := range numberStrings {
+			num, _ := strconv.Atoi(s)
+			currentLineNumbers = append(currentLineNumbers, num)
+		}
+
+		numericData.Numbers = append(numericData.Numbers, currentLineNumbers)
 	}
 
 	fmt.Printf("Solution to part 1: %v\n", solutions.PartOne(numericData))
+	fmt.Printf("Solution to part 2: %v\n", solutions.PartTwo(numericData))
 }
--- 2025/day-06/solutions/part2.go
+++ 2025/day-06/solutions/part2.go
@@ -1,6 +1,32 @@
 package solutions
 
-func PartTwo(records []string) (int, error) {
+func PartTwo(numericData Calculation) int {
+	totalValue := 0
 
-	return 0, nil
+	numColumns := len(numericData.Numbers[0])
+
+	for columnIndex := range numColumns {
+		nums := make([]int, 0)
+
+		for i := range numericData.Numbers {
+			nums = append(nums, numericData.Numbers[i][columnIndex])
+		}
+		totalValue += calculateValueTwo(nums, numericData.Operations[columnIndex])
+	}
+
+	return totalValue
+}
+
+func calculateValueTwo(numbers []int, operation string) int {
+	result := 0
+
+	for i := 0; i < len(numbers)-3; i++ {
+		switch operation {
+		case "+":
+			result += (numbers[i] + numbers[i+1] + numbers[i+2] + numbers[i+3])
+		case "*":
+			result += (numbers[i] * numbers[i+1] * numbers[i+2] * numbers[i+3])
+		}
+	}
+	return result
 }
